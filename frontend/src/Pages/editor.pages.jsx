0f39619c88aa806f58b51a1aa84a8a22153a952e
Anujbansal527 <anujbansal527@gmail.com>
2024-02-29 03:10:32 +0530

    adding editor js in our blog page

--- frontend/src/Pages/editor.pages.jsx
+++ frontend/src/Pages/editor.pages.jsx
@@ -1,25 +1,42 @@
 
 
 
-import React, { useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import { UserContext } from '../App'
 import { Navigate } from 'react-router-dom'
 import BlogEditor from '../Components/blog-editor.component'
 import PublishForm from '../Components/publish-form.component'
 
+const blogStructure = {
+    title: '',
+    banner: '',
+    content: [],
+    tags:[],
+    des:'',
+    author: {
+        personal_info : { }
+    }
+}
+
+export const EditorContext = createContext({});
+
 const Editor = () => {
+
+    const [blog,setBlog] = useState(blogStructure)
+
+    const [editorState,setEditorState] = useState("editor")
 
     let { userAuth : {access_token} } = useContext(UserContext)
  
-    const [editorState,setEditorState] = useState("editor")
-
     return (
         <>
+        <EditorContext.Provider value={ {blog , setBlog , editorState ,setEditorState}}>
             {
                 access_token === null ? <Navigate to={"/singin"} />
                 : editorState == "editor" ? <BlogEditor/> : <PublishForm/> 
             
             }
+        </EditorContext.Provider>
         </>
   )
 }
